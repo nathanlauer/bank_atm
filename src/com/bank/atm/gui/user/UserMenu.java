ca014a1aaa9c30a7130d7d24c67d1b60c42ee362
Sandra Zhen <szhen8930@gmail.com>
2020-12-02 23:37:34 -0500

    added buttons for user interface

--- src/com/bank/atm/gui/user/UserMenu.java
+++ src/com/bank/atm/gui/user/UserMenu.java
@@ -1,4 +1,38 @@
 package com.bank.atm.gui.user;
 
-public class UserMenu {
+/**
+ * @author Sandra Zhen
+ * Class represents user menu interface when user first logs in
+ */
+import javax.swing.*;
+
+public class UserMenu extends JFrame {
+
+    private JPanel menuPanel;
+    private JLabel usernameLabel;
+    private JButton viewAccountsButton;
+    private JButton depositButton;
+    private JButton withdrawButton;
+    private JButton transferMoneyButton;
+    private JButton loansButton;
+
+    public UserMenu(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(menuPanel);//sets content to our menu panel
+        this.pack();
+        usernameLabel.setText(getUserName());
+    }
+
+    private String getUserName(){
+        //TODO retrieve and return username from oauth
+        return "Welcome "+"Name"+"!";
+    }
+    //TODO delete later. this for testing purposes
+    public static void main(String [] args){
+        JFrame frame = new UserMenu("User Menu");
+        frame.setVisible(true);
+    }
+
 }
