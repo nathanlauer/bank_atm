6a8a9159d743aca086a1d1a05ad11950b1e3284d
Nathan Lauer <lauern91@gmail.com>
2020-12-09 18:11:26 -0500

    Added basic currency structure

---
+++ src/com/bank/atm/backend/currency/Currency.java
@@ -0,0 +1,14 @@
+package com.bank.atm.backend.currency;
+
+/**
+ * Class Currency is an abstract class which represents some currency.
+ *
+ * @author: Nathan Lauer
+ * @email: lauern@bu.edu
+ * Creation Date: 12/9/20
+ * <p>
+ * Please feel free to ask me any questions. I hope you're having a nice day!
+ */
+public abstract class Currency {
+
+}
