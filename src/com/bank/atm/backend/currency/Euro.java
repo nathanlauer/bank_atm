d7808aeb0c5647577aeec133544a77f995acb03d
Nathan Lauer <lauern91@gmail.com>
2020-12-10 16:37:11 -0500

    Small updates to USD class, and added classes Euro and JPY

--- src/com/bank/atm/backend/currency/Euro.java
+++ src/com/bank/atm/backend/currency/Euro.java
@@ -3,33 +3,32 @@ package com.bank.atm.backend.currency;
 import java.text.DecimalFormat;
 
 /**
- * Class USD represents the US Dollar as a Currency.
+ * Class Euro represents the Euro as a Currency
  *
  * @author: Nathan Lauer
  * @email: lauern@bu.edu
- * Creation Date: 12/9/20
+ * Creation Date: 12/10/20
  * <p>
  * Please feel free to ask me any questions. I hope you're having a nice day!
  */
-public class USD extends Currency {
-    private static USD instance;
+public class Euro extends Currency {
+    private static Euro instance;
 
     /**
      * Each currency is implemented as a Singleton, so this abstract method
      * helps to enforce that.
+     *
      * @return the instance representing the relevant Currency.
      */
+    @Override
     public Currency getInstance() {
         if(instance == null) {
-            instance = new USD();
+            instance = new Euro();
         }
         return instance;
     }
 
-    /**
-     * Private constructor.
-     */
-    private USD() {
+    private Euro() {
         // Empty, but private to prevent outside access.
     }
 
@@ -37,12 +36,12 @@ public class USD extends Currency {
      * Displays the passed in Money as expected for the given Currency.
      *
      * @param money the Money to be displayed
-     * @return String representation of the Money. For example, $13,456.89
+     * @return String representation of the Money
      */
     @Override
     public String displayMoney(Money money) {
         DecimalFormat formatter = new DecimalFormat("#,###.00");
-        return "$" + formatter.format(money.getAmount());
+        return "\u20AC" + formatter.format(money.getAmount());
     }
 
     /**
@@ -66,6 +65,6 @@ public class USD extends Currency {
      */
     @Override
     public String toString() {
-        return "USD";
+        return "EUR";
     }
 }
--- src/com/bank/atm/backend/currency/JPY.java
+++ src/com/bank/atm/backend/currency/JPY.java
@@ -3,46 +3,45 @@ package com.bank.atm.backend.currency;
 import java.text.DecimalFormat;
 
 /**
- * Class USD represents the US Dollar as a Currency.
+ * Class JPY represents Japanese Yen as a Currency.
  *
  * @author: Nathan Lauer
  * @email: lauern@bu.edu
- * Creation Date: 12/9/20
+ * Creation Date: 12/10/20
  * <p>
  * Please feel free to ask me any questions. I hope you're having a nice day!
  */
-public class USD extends Currency {
-    private static USD instance;
+public class JPY extends Currency {
+    private static JPY instance;
 
     /**
      * Each currency is implemented as a Singleton, so this abstract method
      * helps to enforce that.
+     *
      * @return the instance representing the relevant Currency.
      */
+    @Override
     public Currency getInstance() {
         if(instance == null) {
-            instance = new USD();
+            instance = new JPY();
         }
         return instance;
     }
 
-    /**
-     * Private constructor.
-     */
-    private USD() {
-        // Empty, but private to prevent outside access.
+    private JPY() {
+        // empty, but private to prevent outside access
     }
 
     /**
      * Displays the passed in Money as expected for the given Currency.
      *
      * @param money the Money to be displayed
-     * @return String representation of the Money. For example, $13,456.89
+     * @return String representation of the Money
      */
     @Override
     public String displayMoney(Money money) {
         DecimalFormat formatter = new DecimalFormat("#,###.00");
-        return "$" + formatter.format(money.getAmount());
+        return "\u00A5" + formatter.format(money.getAmount());
     }
 
     /**
@@ -66,6 +65,6 @@ public class USD extends Currency {
      */
     @Override
     public String toString() {
-        return "USD";
+        return "JPY";
     }
 }
