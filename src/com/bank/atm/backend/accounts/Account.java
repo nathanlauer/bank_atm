6dad217f7e81dbfdfc293a440df6e6f17c94a002
Nathan Lauer <lauern91@gmail.com>
2020-12-10 16:28:32 -0500

    Added class AccountValueDecorator, for printing out money in a certain currency

--- src/com/bank/atm/backend/accounts/Account.java
+++ src/com/bank/atm/backend/accounts/Account.java
@@ -1,11 +1,13 @@
 package com.bank.atm.backend.accounts;
 
+import com.bank.atm.backend.currency.Currency;
 import com.bank.atm.backend.currency.Money;
 import com.bank.atm.backend.users.User;
 
-import java.util.Currency;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Class Account
@@ -17,16 +19,75 @@ import java.util.List;
  * Please feel free to ask me any questions. I hope you're having a nice day!
  */
 public class Account {
-    private Date opened;
-    private Currency currency;
-    private Money money;
-    private List<User> managers;
+    private final UUID uuid;
+    private final Date opened;
+    private final Currency currency;
+    private final Money money;
+    private final List<User> managers;
+    private final AccountValueDecorator decorator;
+
+    public Account(Date opened, Currency currency, Money money, List<User> managers) {
+        this.uuid = UUID.randomUUID();
+        this.opened = opened;
+        this.currency = currency;
+        this.money = money;
+        this.managers = new ArrayList<>(managers); // copy to preserve encapsulation
+        this.decorator = new AccountValueDecorator(this);
+    }
+
+    /**
+     *
+     * @return the Currency that this Account uses
+     */
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    /**
+     *
+     * @return the Money object representing the Monetary value of this Account.
+     */
+    public Money getMoney() {
+        return money;
+    }
+
+    /**
+     *
+     * @return the Date that this Account was opened
+     */
+    public Date getOpened() {
+        return opened;
+    }
+
+    /**
+     * Adds an authorized User to this Account
+     * @param manager the new User to be authorized as a manager for this Account.
+     */
+    public void addManager(User manager) {
+        managers.add(manager);
+    }
+
+    /**
+     * Removes a User from being an authorized manager of this Account
+     * @param manager the User to be removed
+     */
+    public void removeManager(User manager) {
+        managers.remove(manager);
+    }
+
+    /**
+     *
+     * @return a String representing the value of this Account. For example: $1,897.23
+     */
+    public String displayAccountValue() {
+        return decorator.displayAccountValue();
+    }
 
     /**
      * Defines equality for two Account objects.
      *
      * @param o Other object in consideration for equality
-     * @return true if o is an instance of Account, and TODO
+     * @return true if o is an instance of Account, and they have the same UUID.
      */
     @Override
     public boolean equals(Object o) {
@@ -39,7 +100,6 @@ public class Account {
         }
 
         Account other = (Account) o;
-        // TODO
-        return false;
+        return this.uuid.equals(other.uuid);
     }
 }
---
+++ src/com/bank/atm/backend/accounts/AccountValueDecorator.java
@@ -0,0 +1,36 @@
+package com.bank.atm.backend.accounts;
+
+import com.bank.atm.backend.currency.Money;
+
+/**
+ * Class AccountValueDecorator is a class which acts as a Decorator for displaying the
+ * value in an account. It is expected to be composed within an Account, so the nominal
+ * usage is something like:
+ * String output = account.displayAccountValue();
+ * where the account internally uses this class to actually handle the display semantics.
+ *
+ * @author: Nathan Lauer
+ * @email: lauern@bu.edu
+ * Creation Date: 12/10/20
+ * <p>
+ * Please feel free to ask me any questions. I hope you're having a nice day!
+ */
+public class AccountValueDecorator {
+    private final Account account;
+
+    /**
+     * Standard constructor
+     * @param account the Account that this class decorates
+     */
+    public AccountValueDecorator(Account account) {
+        this.account = account;
+    }
+
+    /**
+     * Displays the value in the account using using the Account's currency
+     * @return String representing the Account's value, for example: $13,453.23
+     */
+    public String displayAccountValue() {
+        return account.getCurrency().displayMoney(account.getMoney());
+    }
+}
