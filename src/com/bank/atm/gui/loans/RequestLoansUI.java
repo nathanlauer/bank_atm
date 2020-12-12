a59d73b2b42d4f5c479cc0acfa6b58508771841f
Sandra Zhen <szhen8930@gmail.com>
2020-12-11 20:25:17 -0500

    created basic interface for request loans ui

--- src/com/bank/atm/gui/loans/RequestLoansUI.java
+++ src/com/bank/atm/gui/loans/RequestLoansUI.java
@@ -3,4 +3,7 @@ package com.bank.atm.gui.loans;
 import javax.swing.*;
 
 public class RequestLoansUI extends JFrame {
+    private JFormattedTextField loanAmountRequested;
+    private JFormattedTextField formattedTextField1;
+    private JButton requestLoanButton;
 }
