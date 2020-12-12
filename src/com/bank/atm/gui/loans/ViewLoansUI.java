c8cf57bdca02c6497b1f231f10636a310d66fcba
Sandra Zhen <szhen8930@gmail.com>
2020-12-12 13:21:17 -0500

    added loans ui display

--- src/com/bank/atm/gui/loans/ViewLoansUI.java
+++ src/com/bank/atm/gui/loans/ViewLoansUI.java
@@ -3,4 +3,8 @@ package com.bank.atm.gui.loans;
 import javax.swing.*;
 
 public class ViewLoansUI extends JFrame {
+    private JScrollPane currentLoansScrollPane;
+    private JScrollPane pendingLoansScrollPane;
+    private JPanel currentLoansPanel;
+    private JPanel pendingLoansPanel;
 }
