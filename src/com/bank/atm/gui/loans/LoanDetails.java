8206dfb497062b1f5e38753fbd2eda3faa1314e0
Sandra Zhen <szhen8930@gmail.com>
2020-12-14 13:21:39 -0500

    created account details with account info loaded

---
+++ src/com/bank/atm/gui/loans/LoanDetails.java
@@ -0,0 +1,12 @@
+package com.bank.atm.gui.loans;
+
+import com.bank.atm.backend.accounts.loan_accounts.LoanAccount;
+
+import javax.swing.*;
+
+public class LoanDetails extends JFrame{
+    private JPanel loanDetailsPanel;
+    public LoanDetails(LoanAccount loanAccount){
+
+    }
+}
