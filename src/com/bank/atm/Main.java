ef3c50728a264240abc3ce6aae686b97f812bf27
Nathan Lauer <lauern91@gmail.com>
2020-12-15 18:09:44 -0500

    Added new calss DefaultAdmin, and run it in main

--- src/com/bank/atm/Main.java
+++ src/com/bank/atm/Main.java
@@ -1,5 +1,6 @@
 package com.bank.atm;
 
+import com.bank.atm.backend.authentication.DefaultAdmin;
 import com.bank.atm.gui.login.Login;
 
 import javax.swing.*;
@@ -7,6 +8,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        DefaultAdmin.run();
         JFrame frame = new Login("Login");
         frame.setVisible(true);
     }
